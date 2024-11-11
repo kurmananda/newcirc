37c824edc4e654e45f23c7b4e67b7986792c4096
Kurmananda Pavan Sai Buragana <edu.kurmananda@gmail.com>
2024-11-11 09:31:40 +0530

    Update layout.jsx

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -1,11 +1,12 @@
 export const metadata = {
-  title: "Study Circuit",
+   title: "Study Circuit",
   description:"an initiative to help students",
   icons: {
-    icon: "images/icon.png", // /public path},
-  url: "https://dhanak.iist.org.in",
+    icon: "images/icon.png", // /public path
+  },
+  url: "https://studycircuit.vercel.app",
   type: "website",
-  siteName: "Dhanak 2k25",
+  siteName: "StudyCirc",
 };
 
 export default function HomeLayout({ children }) {
