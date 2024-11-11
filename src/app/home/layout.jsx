0f6a9e13fd16c24e45bc6dfd0e534b56ca21d33b
Kurmananda Pavan Sai Buragana <edu.kurmananda@gmail.com>
2024-11-11 09:30:46 +0530

    Update layout.jsx

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -1,10 +1,8 @@
 export const metadata = {
-  title: "GATE Mock",
-  description:
-    "Dhanak 2k25 is a cultural fest organised by the students of Indian Institute of Space Science and Technology, Thiruvananthapuram.",
+  title: "Study Circuit",
+  description:"an initiative to help students",
   icons: {
-    icon: "icon.png", // /public path
-  },
+    icon: "images/icon.png", // /public path},
   url: "https://dhanak.iist.org.in",
   type: "website",
   siteName: "Dhanak 2k25",
