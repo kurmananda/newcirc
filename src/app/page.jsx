7afe9c5936596b352f42cb8f3610d57b94f62338
Kurmananda Pavan Sai Buragana <edu.kurmananda@gmail.com>
2024-11-11 12:49:32 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export const metadata = {
   title: "Study Circuit",
   description:"an initiative to help students",
   icons: {
-    icon: "images/icon.png", // /public path
+    icon: "images/logo_half.png", // /public path
   },
   url: "https://studycircuit.vercel.app",
   type: "website",
